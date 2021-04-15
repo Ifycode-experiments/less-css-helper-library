be12eb2d74bd434ad19189b3429f09d67b246808
ifycode <marychord@gmail.com>
2021-04-14 21:43:16 -0800

    Configure express to open up index.html

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,10 +2,22 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
-
 const port = 3000;
 const app = express();
 
-const compiler = webpack(config);
+/*eslint-disable no-console*/
+
+app.use(express.static('src'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Open: http://localhost:${port}`);
+    open(`http://localhost:${port}`);
+  }
+});
